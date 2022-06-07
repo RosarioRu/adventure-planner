2e056f238735991f99b588e0720e7b27729074ea
RosarioRu <mariarruval@gmail.com>
2022-06-06 20:45:13 -0700

    Udate path for files in webpack.

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/styles.css';
+import '../css/styles.css';
 // import TemplateClassName from './js/template.js';
 
 $(document).ready(function() {
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/js/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
