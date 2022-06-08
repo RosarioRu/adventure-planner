0c34ded6c767ea5a46ec3cf4d201c445363e3e85
Patty Otero <101524326+patty-rose@users.noreply.github.com>
2022-06-07 22:05:28 -0700

    correct date feature

--- src/index.js
+++ src/index.js
@@ -14,26 +14,22 @@ $(document).ready(function() {
   $('#begin').submit(function (event) {
     event.preventDefault();
     const user = $('#user').val();
-    const tripTypeValue = $('#tripType').val();
-    console.log(tripTypeValue);
-    console.log(user);
+    // const tripTypeValue = $('#tripType').val();
     currentUser = new Person(user);
-    console.log(currentUser);
     // window.location.href='tripForm.html';
   });
 
   $('#tripCreate').on("click", function (event) {
     event.preventDefault();
-    console.log("hi");
     const tripName = $('#tripName').val();
     const tripDestination = $('#tripDestination').val();
     const tripParticipants = $('#tripParticipants').val();
-    const tripStartDate = $('#tripStartDate').val();
-    const tripEndDate = $('#tripEndDate').val();
+    const tripStartDate = $('#startDate').val();
+    const tripEndDate = $('#endDate').val();
     currentTrip = new Trip("backpacking", tripName, tripParticipants, tripDestination, tripStartDate, tripEndDate);
     currentUser["trips"].push(currentTrip);
-    console.log(currentTrip);
-    console.log(currentUser);
+    console.log(tripStartDate);
+    console.log(tripEndDate);
   });
 
   $('#foodPlanning').on("click", function(event) {
@@ -44,12 +40,8 @@ $(document).ready(function() {
     let as = $('#as').val();
     let din = $('#din').val();
     let foodDay1 = {"breakfast" : bf, "morning_snack" : ms, "lunch" :  lunch, "afternoon_snack" : as, "dinner" : din};
-    console.log(bf);
-    console.log(foodDay1);
     let tripFood = new FoodSchedule(foodDay1);
     currentTrip.food = tripFood;
-    console.log(currentUser);
-    console.log(currentTrip);
   });
 
 });
